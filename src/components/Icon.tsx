6e2e84fc1fd64f41f084e3be33f2a4399234f056
captain-Akshay <akshay.sharma0503@gmail.com>
2023-07-18 04:26:22 +0530

    few changes

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,7 +2,7 @@ import { LucideProps, MessageSquare, User } from 'lucide-react'
 
 export const Icons = {
   user: User,
-  login:(props: LucideProps) => (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key"><circle cx="7.5" cy="15.5" r="5.5"/><path d="m21 2-9.6 9.6"/><path d="m15.5 7.5 3 3L22 7l-3-3"/></svg>),
+  login:() => (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-key"><circle cx="7.5" cy="15.5" r="5.5"/><path d="m21 2-9.6 9.6"/><path d="m15.5 7.5 3 3L22 7l-3-3"/></svg>),
   logo: (props: LucideProps) => (
     <svg {...props} viewBox='0 0 497 497'>
       <g>
