733920b096d28ec6418d915801cdac7a840b00c4
captain-Akshay <akshay.sharma0503@gmail.com>
2023-07-18 06:04:40 +0530

    few changes

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,8 +1,32 @@
+import { User } from "@prisma/client";
 import BellNotification from "./BellNotification";
-import axios from "axios";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
 
 const Notification= async ({})=>{
-  const {data}= await axios.get('http://localhost:3000/api/friendsrequestgot');
-  return (<BellNotification data={data}/>)
+  const session = await getAuthSession();
+    const sessionUserId=session?.user.id
+      // Get all friend requests received by the session user
+      const friendRequests = await db.friendRequest.findMany({
+        where: {
+          receiverId: sessionUserId,
+        },
+      });
+  
+      // Create an array to store the User objects
+      const users: User[] = [];
+  
+      // Iterate over the friend requests and fetch the sender details
+      for (const request of friendRequests) {
+        const sender = await db.user.findUnique({
+          where: {
+            id: request.senderId,
+          },
+        });
+  
+        if (sender) {
+          users.push(sender);
+        }}
+  return (<BellNotification data={users}/>)
 }
 export default Notification;
